2a6dcd2eca642a19a3c2d7013df303dab4b15d84
unknown <renato.galindo@hotmail.com>
2020-07-11 11:21:33 -0300

    ajuste na uri

--- db.js
+++ db.js
@@ -5,9 +5,10 @@ class Database {
     };
     connectMongo() {
         this.mongoose
-            .connect('mongodb+srv://chatbot:masterik61703@cluster0-wegmx.mongodb.net/chatbot')
+      
+            .connect('mongodb+srv://chatbot:masterik61703@cluster0-wegmx.mongodb.net/chatbot?retryWrites=true&w=majority')
             .then(() => console.log(`Conectado a base de dados`))
-            .catch(err => console.error('Falha ao tentar conexao com a base de dados' + err))
+            .catch(err => console.error('Falha ao tentar conexao com a base de dados ' + 'URI ' + 'mongodb+srv://chatbot:masterik61703@cluster0-wegmx.mongodb.net/chatbot?retryWrites=true&w=majority' + err))
     }
 }
 module.exports = new Database();
