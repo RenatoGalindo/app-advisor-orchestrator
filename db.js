05d98051fcbeeafa3f313a50ecf5cd4313793820
unknown <renato.galindo@hotmail.com>
2020-07-11 11:06:07 -0300

    erro no banco

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ class Database {
         this.mongoose
             .connect('mongodb+srv://chatbot:masterik61703@cluster0-wegmx.mongodb.net/chatbot')
             .then(() => console.log(`Conectado a base de dados`))
-            .catch(err => console.error('Falha ao tentar conexao com a base de dados'))
+            .catch(err => console.error('Falha ao tentar conexao com a base de dados' + err))
     }
 }
 module.exports = new Database();
